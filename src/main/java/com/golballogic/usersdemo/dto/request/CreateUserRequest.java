4d74547bdb997028464788a690efd4a02df0496e
Andres Julian Gomez <ajgomez@hexacta.com>
2023-10-23 11:44:02 -0500

    Adding email and password validation

--- src/main/java/com/golballogic/usersdemo/dto/request/CreateUserRequest.java
+++ src/main/java/com/golballogic/usersdemo/dto/request/CreateUserRequest.java
@@ -1,6 +1,7 @@
 package com.golballogic.usersdemo.dto.request;
 
 import com.golballogic.usersdemo.domain.User;
+import com.sun.istack.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -8,7 +9,9 @@ import java.util.List;
 @Data
 public class CreateUserRequest {
     private String name;
+    @NotNull
     private String email;
+    @NotNull
     private String password;
     private List<PhoneDTO> phones;
 
@@ -16,7 +19,7 @@ public class CreateUserRequest {
         User user = new User();
         user.setActive(true);
         user.setEmail(this.email);
-        user.setName(this.email);
+        user.setName(this.name);
         user.setPassword(this.password);
         return user;
     }
--- src/main/java/com/golballogic/usersdemo/service/UserService.java
+++ src/main/java/com/golballogic/usersdemo/service/UserService.java
@@ -5,16 +5,21 @@ import com.golballogic.usersdemo.domain.User;
 import com.golballogic.usersdemo.dto.request.CreateUserRequest;
 import com.golballogic.usersdemo.dto.request.PhoneDTO;
 import com.golballogic.usersdemo.dto.response.UserCreationResponse;
+import com.golballogic.usersdemo.exception.UserCreationException;
 import com.golballogic.usersdemo.repository.PhoneRepository;
 import com.golballogic.usersdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
+    private static final String PASSWORD_REGEX = "^(?=(?:[^A-Z]*[A-Z]){1})(?=(?:[^0-9]*[0-9]){2})[A-Za-z0-9]{8,12}$";
     private final UserRepository userRepository;
     private final PhoneRepository phoneRepository;
 
@@ -25,6 +30,9 @@ public class UserService {
     }
 
     public UserCreationResponse saveUser(CreateUserRequest userRequest) {
+        validateEmail(userRequest.getEmail());
+        validatePasswordFormat(userRequest.getPassword());
+
         User savedUser = userRepository.save(userRequest.toUserEntity());
 
         List<Phone> phones = userRequest.getPhones().stream()
@@ -48,5 +56,24 @@ public class UserService {
 
         return userResponse;
 
+    }
+    
+    private void validateEmail(String emailAddress) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(emailAddress);
+
+        if(!matcher.matches()) {
+            throw new UserCreationException("Email address is invalid");
+        };
+    }
+    
+    //TODO need to fix regex to allow only one capital letter and 2 numbers
+    private void validatePasswordFormat(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+        Matcher matcher = pattern.matcher(password);
+
+        if(!matcher.matches()) {
+            throw new UserCreationException("Password is invalid");
+        }
     }
 }
